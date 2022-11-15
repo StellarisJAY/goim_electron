40dec7fbf677def2979bf06861135c100ba84538
StellarisJAY <760409558@qq.com>
2022-11-15 14:56:37 +0800

    feat: chat page init

---
+++ src/pages/chat.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <a-row>
+        <a-col :span="6">
+          <SideBar/>
+        </a-col>
+      </a-row>
+    </div>
+  </template>
+  
+  <script>
+  import {Row, Col} from 'ant-design-vue'
+  import SideBar from '../components/sidebar/Sidebar.vue'
+  export default {
+    name: 'ChatPage',
+    components: {
+      SideBar,
+      ARow: Row,
+      ACol: Col
+    },
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+      }
+    },
+    methods: {
+      
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  
+  </style>
+  
