1255a6f00a2f948f384c20ec484900b53f13e811
StellarisJAY <760409558@qq.com>
2022-11-17 16:15:43 +0800

    feat: 聊天界面用户信息加载

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -8,7 +8,7 @@
                 <template #extra>
                     <a><PhoneOutlined/></a>
                 </template>
-                <MessageBox :messages="messages" id="message-box"/>
+                <MessageBox :messages="messages" :group-members="groupMembers" :other-user="userInfo" id="message-box"/>
                 <a-divider></a-divider>
                 <a-textarea placeholder="input message..." id="text-area"></a-textarea>
                 <a-button type="primary" id="send-button">发送</a-button>
@@ -16,6 +16,8 @@
         </a-col>
         <a-col :span="6">
             <a-card id="info-card" hoverable="true">
+                {{groupInfo}}
+                {{groupMembers}}
             </a-card>
         </a-col>
     </a-row>
@@ -24,6 +26,7 @@
 <script>
 import {Card, Col, Row, Textarea, Button, Divider} from 'ant-design-vue'
 import { PhoneOutlined } from '@ant-design/icons-vue';
+import {get} from '../../api/request.js'
 import MessageBox from './MessageBox.vue';
 export default {
     name: 'ChatCard',
@@ -43,13 +46,92 @@ export default {
     },
     data() {
         return {
-            messages: []
+            groupInfo: null,
+            userInfo: null,
+            groupMembers: new Map,
+            messages: [
+                {id: 1, content: 'hello world', from: 1001, to: 58241228567105540},
+                {id: 1, content: 'hello world', from: 1001, to: 58241228567105540},
+                {id: 1, content: 'hello world', from: 58241228567105540, to: 1001},
+                {id: 1, content: 'hello world', from: 58241228567105540, to: 1001},
+                {id: 1, content: 'hello world', from: 1001, to: 58241228567105540},
+                {id: 1, content: 'hello world', from: 1001, to: 58241228567105540},
+                {id: 1, content: 'hello world', from: 58241228567105540, to: 1001},
+                {id: 1, content: 'hello world', from: 1001, to: 58241228567105540},
+                {id: 1, content: 'hello world', from: 58241228567105540, to: 1001},
+                {id: 1, content: 'hello world', from: 1001, to: 58241228567105540},
+            ]
         }
     },
     methods: {
-        
+        getUserInfo: async function(id) {
+            try {
+                let response = await get('/user/' + id)
+                let resp = response.data
+                if (resp.code == 404) {
+                    console.error("unexpected error user not found")
+                    return null
+                }else {
+                    return resp.userInfo
+                }
+            }catch(error) {
+                console.error(error)
+                return null
+            }
+        },
+        listGroupMembers: async function(groupID) {
+            try {
+                let response = await get('/group/members/' + groupID)
+                let resp = response.data
+                if (resp.code != 200) {
+                    console.error(resp.message)
+                    return null
+                }else {
+                    let members = resp.members
+                    let result = new Map
+                    for (let member in members) {
+                        result.set(member.id, member)
+                    }
+                    return result
+                }
+            }catch(error) {
+                console.error(error)
+                return null
+            }
+        },
+        getGroupInfo: async function(groupID) {
+            try {
+                let response = await get('/group/' + groupID)
+                let resp = response.data
+                if (resp.code != 200) {
+                    console.error(resp.message)
+                    return null
+                }else {
+                    return resp.groupInfo
+                }
+            }catch(error) {
+                console.error(error)
+                return null
+            }
+        }
     },
-    mounted() {
+    created() {
+        let that = this
+        if (!this.isGroup) {
+            this.getUserInfo(this.target)
+            .then(userInfo=>{
+                that.userInfo = userInfo
+            })
+        }else {
+            this.getGroupInfo(this.target)
+            .then(group=>{
+                that.groupInfo = group
+            })
+            this.listGroupMembers(this.target)
+            .then(members=>{
+                that.groupMembers = members
+            })
+        }
     }
 }
 </script>
